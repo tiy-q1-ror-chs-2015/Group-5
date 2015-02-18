2eb6cae6b8e7267e4d6e1d69936a1ed263045b96
Matthew Gallo <mdgallo07@gmail.com>
2015-02-18 15:14:53 -0500

    Added devise views and changed to haml.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me, ) } 
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :breed, :age, :gender, :photo, :description) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :breed, :age, :gender, :photo, :description) }
 end
